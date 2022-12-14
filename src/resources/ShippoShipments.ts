da0da7d1d008384485ff6bbf094972ffd937c646
DaddyWarbucks <techtbeau@gmail.com>
2022-12-13 20:39:08 -0600

    Fix comment

--- src/resources/ShippoShipments.ts
+++ src/resources/ShippoShipments.ts
@@ -11,18 +11,18 @@ export class ShippoShipments extends ShippoService {
     super(options, app);
   }
 
-//   get(id: ID, params?: Params) {
-//     if (!id) {
-//       throw new BadRequest('ID is required');
-//     }
-//     if (params?.query?.rates) {
-//       return this.schedule('get', () => {
-//         return this.shippo
-//           .get(`shipments/${id}/rates/${params?.query?.rates}`)
-//           .then(this.handleResult)
-//           .catch(this.handleError);
-//       });
-//     }
-//     return super.get(id, params);
-//   }
-// }
+  // get(id: ID, params?: Params) {
+  //   if (!id) {
+  //     throw new BadRequest('ID is required');
+  //   }
+  //   if (params?.query?.rates) {
+  //     return this.schedule('get', () => {
+  //       return this.shippo
+  //         .get(`shipments/${id}/rates/${params?.query?.rates}`)
+  //         .then(this.handleResult)
+  //         .catch(this.handleError);
+  //     });
+  //   }
+  //   return super.get(id, params);
+  // }
+}
